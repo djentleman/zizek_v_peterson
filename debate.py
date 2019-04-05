8d8b7621760b7910b09fe61069fb5ec10282708c
Todd Perry <todd.perry@myport.ac.uk>
2019-04-05 19:05:49 +0100

    add simple rebuttle function

--- debate.py
+++ debate.py
@@ -1,15 +1,25 @@
 from debator import (
     Debator
 )
+import random
 
 def main():
-    zizek = Debator('Zizek', 'corpus/zizek.txt')
-    peterson = Debator('Peterson', 'corpus/peterson.txt')
-    for i in range(10):
-        print(zizek.generate_point())
-    print('---')
-    for i in range(10):
-        print(peterson.generate_point())
+    debators = [
+        Debator('Zizek', 'corpus/zizek.txt'),
+        Debator('Peterson', 'corpus/peterson.txt')
+    ]
+    turns = 10
+    for i in range(turns):
+        debator = debators[i % len(debators)]
+        if i == 0:
+            argument = debator.generate_point(random.randint(1, 3))
+            print('%s%s: %s' % (('\t\t\t\t' if i % len(debators) == 1 else ''), debator.name, argument))
+        else:
+            argument = debator.rebuttal(argument, random.randint(1, 3))
+            print('%s%s: %s' % (('\t\t\t\t' if i % len(debators) == 1 else ''), debator.name, argument))
+        print('')
+
+
 
 if __name__ == '__main__':
     main()
--- debator.py
+++ debator.py
@@ -3,6 +3,14 @@ from settings import (
     MARKOV_CHAIN_STATE_SIZE
 )
 
+def get_similarity(str1, str2):
+    # return the percentage of the strings that overlap
+    a = set(str1.lower().split(' '))
+    b = set(str2.lower().split(' '))
+    intersect = a.intersection(b)
+    return float(len(intersect)) / (len(a) + len(b) - len(intersect))
+
+
 class Debator():
 
     def __init__(self, name, corpus):
@@ -10,7 +18,16 @@ class Debator():
         training_data = open(corpus).read()
         self.markov_model = markovify.Text(training_data, state_size=MARKOV_CHAIN_STATE_SIZE)
 
-    def generate_point(self):
-        point = self.markov_model.make_sentence()
+    def generate_point(self, count=1):
+        point = ' '.join([self.markov_model.make_sentence()
+                          for i in range(count)])
         return point
+
+    def rebuttal(self, argument, count=1):
+        counter_argument_pool_size = 10000
+        potential_counter_arguments = [self.markov_model.make_sentence()
+                                      for i in range(counter_argument_pool_size)]
+        counter_arguments = sorted(potential_counter_arguments, key=lambda x: -get_similarity(x, argument))[:count]
+        return ' '.join(counter_arguments)
+
 
