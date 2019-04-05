d72310199320103b9f021cd7f66e75ad01fdb3ef
Todd Perry <todd.perry@myport.ac.uk>
2019-04-05 19:17:56 +0100

    improve similaroty algo

--- debator.py
+++ debator.py
@@ -2,13 +2,21 @@ import markovify
 from settings import (
     MARKOV_CHAIN_STATE_SIZE
 )
+from nltk.corpus import (
+    stopwords
+)
+
+en_stopwords = stopwords.words('english')
 
 def get_similarity(str1, str2):
+    # remove stop words
+    str1 = [word for word in str1.lower().split(' ') if word not in en_stopwords]
+    str2 = [word for word in str2.lower().split(' ') if word not in en_stopwords]
     # return the percentage of the strings that overlap
-    a = set(str1.lower().split(' '))
-    b = set(str2.lower().split(' '))
+    a = set(str1)
+    b = set(str2)
     intersect = a.intersection(b)
-    return float(len(intersect)) / (len(a) + len(b) - len(intersect))
+    return float(len(intersect)) / (len(a) + len(b))
 
 
 class Debator():
@@ -27,6 +35,9 @@ class Debator():
         counter_argument_pool_size = 10000
         potential_counter_arguments = [self.markov_model.make_sentence()
                                       for i in range(counter_argument_pool_size)]
+        potential_counter_arguments = [argument
+                                      for argument in potential_counter_arguments
+                                      if argument != None]
         counter_arguments = sorted(potential_counter_arguments, key=lambda x: -get_similarity(x, argument))[:count]
         return ' '.join(counter_arguments)
 
